d28199fb85f65781300e7fc5863854a2886913e9
Joel Muthiani <carptainjoels002@gmail.com>
2026-03-23 19:40:41 +0300

    Fix create live class URL

--- core/urls.py
+++ core/urls.py
@@ -37,7 +37,7 @@ urlpatterns = [
     path('exam/<int:exam_id>/add-question/', views.add_question, name='add_question'),
 
     # LIVE SESSIONS
-    path('live/create/', views.create_live_session, name='create_live_class'),
+    path('live/create/<int:course_id>/', views.create_live_session, name='create_live_class'),
     path('live/<int:session_id>/', views.live_session_room, name='live_room'),
     path('live/end/<int:session_id>/', views.end_live_session, name='end_live_session'),
     path('live/join/<int:session_id>/', views.join_live_class, name='join_live_class'),
